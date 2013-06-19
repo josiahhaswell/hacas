5addf190280d39c4a08f324ef5121cdf0d93ef50
= <=>
2013-06-19 00:49:01 -0600

    Modularizing stuff

---
+++ Expression.hs
@@ -0,0 +1,27 @@
+import Hacas.Utils
+module Hacas.Expression where
+data ArithmeticOperation = 
+	Add | Subtract | Divide | Multiply | Exponentiate
+
+instance Show ArithmeticOperation where
+	show Add = "+"
+	show Subtract = "-"
+	show Divide = "/"
+	show Multiply = "*"
+	show Exponentiate = "^"
+
+data Expression = 
+	Numeric Double |
+	Symbol String |
+	Negate Expression | 
+	Function ([Expression] -> Expression) |
+	Arithmetic ArithmeticOperation Expression Expression
+
+instance Show Expression where 
+	show (Numeric a) = (show a)
+	show (Symbol a) = trimQuotes a 
+	show (Function a) = "<function>" 
+	show (Negate a) = "-" ++ (show a)
+	show (Arithmetic op lhs rhs) = 
+		(show lhs) ++ (show op) ++ (show rhs) 
+
---
+++ Utils.hs
@@ -0,0 +1,5 @@
+module Hacas.Utils where
+
+trimQuotes a = 
+	let trimmed = (show a)
+	in (drop 1 $ take (length trimmed - 1) trimmed)
